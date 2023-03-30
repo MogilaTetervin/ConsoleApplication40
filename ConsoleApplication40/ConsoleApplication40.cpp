4309cf25cb33a957782b06270f2f333049b0a817
std <std@RED-STUDENT16>
2023-03-30 20:53:59 +0300

    пвпвапвапва

--- ConsoleApplication40/ConsoleApplication40.cpp
+++ ConsoleApplication40/ConsoleApplication40.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 struct waterkiller {
 	int weight = rand() % 45;
-	int weightcrips = rand() % 2;
+	int weightcrips = rand() % 5;
 };
 void print(waterkiller*& arr, int& size) {
 	for (int i = 0; i < size; i++) {
@@ -11,7 +11,6 @@ void print(waterkiller*& arr, int& size) {
 	}
 	cout << endl;
 }
-
 
 void addkiller(waterkiller*& killer, int& size, waterkiller utopian) {
 	waterkiller* temp = new waterkiller[size + 1];
@@ -32,18 +31,18 @@ waterkiller create(){
 void del(waterkiller*& arr, int& size,int index ) {
 	if (index <= size) {
 		waterkiller* temp = new waterkiller[size - 1];
-		for (int i = 0; i < size; i++) {
+		for (int i = 0; i < index; i++) {
 			temp[i] = arr[i];
 		}
-		for (int i = 0; i < size; i++) {
-			temp[i] = arr[i];
+		for (int j = index + 1; j < size - 1; j++) {
+			temp[j] = arr[j];
 		}
 		delete[] arr;
 		arr = temp;
 		size--;
 	}
 	else
-		cout << "Vlad lox";
+		cout << "Vanya ne cherepashka ninja";
 
 }
 
@@ -51,40 +50,45 @@ int main()
 {
 	srand(time(0));
 	setlocale(LC_ALL, "ru");
-
-	int size = 0;
+	int x;
+	int size = 20;
 	waterkiller* killer = new waterkiller[size];
-
-
+	long costcrips = 0;
+	int costmeat = 0;
 	int meat = 0;
 	int crips = 0;
 	
-	for (int i = 0; i < 20; i++) {
-		waterkiller new_killer = create();
-		addkiller(killer, size, new_killer);
-	}
+	FILE* text;
 
 
 
 	for (int i = 0; i < 31; i++) {
 		for (int j = 0; j < size; j++) {
 			killer[j].weight += 1;
-			killer[j].weight += 25;
+			killer[j].weightcrips += 2;
 		}
-		for (int x = 0; x < 75; x++) {
-			waterkiller new_killer = create();
-			addkiller(killer, size, new_killer);
+		if (i % 5 == 0) {
+			for (int x = 0; x < 5; x++) {
+				waterkiller new_killer = create();
+				addkiller(killer, size, new_killer);
+			}
 		}
 		do
 		{
 			meat += killer[0].weight;
 			crips += killer[0].weightcrips;
-			del(killer, size,5);
+			del(killer, size,1);
 		} while (meat < 45);
 		meat -= 45;
 	}
+	
 	cout << "утопцев  " << size << endl;
 	cout << "утопчина  " << meat << endl;
 	cout << "чешуя  " << crips << endl;
+	costmeat = meat * 273;
+	costcrips = crips * 40;
+	cout << costmeat << "  стоимость мяса" << endl;
+	cout << costcrips << "  стоимость чешуи";
+
 }
 
